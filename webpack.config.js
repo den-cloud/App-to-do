268ef7585d4e85d578ca534ab2b8151efeb5557b
den-cloud <djpants90@yahoo.com>
2021-11-02 13:11:11 +0200

    Loading CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,18 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: {
-        index: './src/index.js',
-        print: './src/print.js',
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Development',
-        }),
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+ module: {
+   rules: [
+     {
+       test: /\.css$/i,
+       use: ['style-loader', 'css-loader'],
+     },
+   ],
+ },
 };
