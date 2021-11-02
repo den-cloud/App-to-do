ae0eb822c7f05b1d77a493a952e7d6f601ece1b9
den-cloud <djpants90@yahoo.com>
2021-11-02 13:19:16 +0200

    New code to index.js & style.css

--- src/index.js
+++ src/index.js
@@ -1,14 +1,55 @@
-import _ from 'lodash';
 import './style.css';
 
- function component() {
-   const element = document.createElement('div');
+function component() {
+  const list = [
+    {
+      description: 'Finish to do list project',
+      completed: false,
+      index: 0,
+    },
+    {
+      description: 'Clean the kitchen',
+      completed: false,
+      index: 0,
+    },
+  ];
 
-   // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  const ul = document.createElement('ul');
+  ul.className = 'list';
+  // Title
+  const title = document.createElement('h2');
+  title.innerHTML = 'Today\'s To Do <i class="fas fa-sync-alt"></i>';
+  title.className = 'title';
+  ul.appendChild(title);
+  // Input
+  const inputContainer = document.createElement('div');
+  const input = document.createElement('input');
+  inputContainer.className = 'input-container';
+  input.type = 'text';
+  input.placeholder = 'Add to your list...';
+  input.className = 'task-input';
+  inputContainer.appendChild(input);
+  inputContainer.innerHTML += '<i class="fas fa-level-down-alt"></i>';
+  ul.appendChild(inputContainer);
+  // Add Btn
+  const btnSubmit = document.createElement('button');
+  btnSubmit.className = 'btn';
+  btnSubmit.textContent = 'Clear all completed';
 
-   return element;
- }
+  list.map((task) => {
+    const li = document.createElement('li');
+    li.id = task.index;
+    li.className = 'list_item';
+    li.innerHTML = `    
+    <input type="checkbox" value=${task.completed}/>
+    <p>${task.description}</p>
+    <i class="fas fa-ellipsis-v"></i>
+    `;
+    return ul.appendChild(li);
+  });
+  ul.appendChild(btnSubmit);
+  return ul;
+}
 
- document.body.appendChild(component());
+const div = document.getElementById('list-container');
+div.appendChild(component());
