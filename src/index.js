867fa0438c5130fb7839c986acf06d8e11208704
den-cloud <djpants90@yahoo.com>
2021-11-09 21:12:50 +0200

    new code to project

--- src/index.js
+++ src/index.js
@@ -5,32 +5,6 @@ import taskStatusChange from './task_status_change.js';
 import addAndRemoveTask from './add-and-remove-task';
 import './style.css';
 
-<<<<<<< HEAD
-const element = document.getElementById('todos-list');
-
-const myTasks = [
-  { description: 'To buy veggies', index: 0, completed: false },
-  { description: 'To meet Erick', index: 1, completed: false },
-  { description: 'To complete Microverse Capstone', index: 2, completed: false },
-];
-
-const addList = (todosList) => {
-  for (let i = 0; i < todosList.length; i += 1) {
-    const listItem = ` <div class="d-flex justify-content-between ps-3 pe-3 align-items-center">
-        <div class="d-flex">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault${i}">
-        <div class="ps-3">${todosList[i].description}</div>
-        </div>
-        <i class="fas fa-ellipsis-v text-secondary" id=""></i>
-      </div>
-      <hr>`;
-
-    element.innerHTML += listItem;
-  }
-};
-
-addList(myTasks);
-=======
 let toDoTasks = [];
 
 const renderList = () => {
@@ -126,4 +100,3 @@ document.addEventListener('click', (e) => {
     renderList();
   }
 });
->>>>>>> Milestone-4
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    index: './src/index.js',
   },
   devServer: {
     static: './dist',
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
