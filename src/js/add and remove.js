210629c87856472f3be089756edcc4f85c4f56e8
den-cloud <djpants90@yahoo.com>
2021-11-09 20:10:49 +0200

    Add and remove.js file

---
+++ src/js/add and remove.js
@@ -0,0 +1,14 @@
+import localStorageController from './__mocks__/localStorage-controller';
+import taskController from './add-and-remove-task';
+
+jest.mock('./localStorage-controller');
+
+describe('to do list testing', () => {
+    test('Add new task', () => {
+        const tasksArr = localStorageController.getDataFromLocalStorage('dum');
+        expect(taskController.addTask(`task${tasksArr.length + 1}`)).toHaveLength(3);
+    });
+    test('Delete task', () => {
+        expect(taskController.deleteTask(0)).toHaveLength(2);
+    });
+});
